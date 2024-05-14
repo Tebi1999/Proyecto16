4fc64fa023f659f9b26ff28fd3bae2248db6a4dc
Tebi1999 <estebansileoni@hotmail.com>
2024-05-14 11:36:29 -0300

    Se agrega formulario de contacto y validacion

---
+++ js/validacion.js
@@ -0,0 +1,44 @@
+function validarCampos() {
+    let nombre = document.querySelector('#nombre').value;
+    let telefono = document.querySelector('#telefono').value;
+    let email = document.querySelector('#email').value;
+
+
+    // expresión regular: permite buscar patrones dentro de una cadena
+
+
+    let soloPalabras = /^[a-z\s]+$/i;
+    let pruebaEmail = /^[a-zA-Z\d\._-]*@[a-zA-Z\d_-]*\.[a-zA-Z]{3}(\.[a-zA-Z]{2})*$/;
+
+
+    let datosValidos = true; 
+    let msjError = 'Alguno de los datos no son validos: ';
+    if (nombre === '' || !soloPalabras.test(nombre)) {
+        datosValidos = false;
+        msjError += ' Debe ingresar un nombre';
+    }
+
+    if (telefono === '' || !Number.isInteger(Number(telefono))) {
+        datosValidos = false;
+        msjError += ' Debe ingresar un numero de telefono';
+    }
+
+    if (email === '') {
+        datosValidos = false;
+        msjError += ' Debe ingresar un correo electronico';
+    }
+
+    let mensaje = document.querySelector('#rtaValidacion');
+    if (datosValidos) {
+        mensaje.innerHTML = 'Los datos se ingresaron correctamente, gracias por su contacto'
+    } else {
+        mensaje.innerHTML = msjError
+    }
+}
+
+
+const formulario = document.querySelector('#formContacto');
+formulario.addEventListener('submit', evento => {
+    validarCampos();
+    evento.preventDefault();
+});
